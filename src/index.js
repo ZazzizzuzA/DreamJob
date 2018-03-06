168c1e6c7223e1152e904d906b292aba027eaf0d
Mykhailo Pylypenko <musicvyp@gmail.com>
2018-03-06 23:48:06 +0200

    Проблемы со сборкой. Узнать про Eslint, почему ругается на import styles.scss из index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,2 @@
-export default {
-	data: {
-		h1: "Hello, World!",
-	},
-	method: {
-		append: function() {
-			let body = document.getElementsByTagName("body")[0];
-			body.appendChild(this.h1);
-		}
-	}
-}
+import "./styles/styles";
+console.log("Hello");
--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,50 @@
-import path from ("path");
-import webpack from ("webpack");
-import HtmlWebpackPlugin from ("html-webpack-plugin");
+const path = require("path"),
+    webpack = require("webpack"),
+    HtmlWebpackPlugin = require("html-webpack-plugin");
 
-export default {
-	entry: {
-		main: path.resolve(__dirname, "src", "index.js"),
-	},
-	output: {
-		filename: "[name].js",
-		path: path.resolve(__dirname, "dist"),
-	},
-	resolve: {
-		extensions: [ ".js", ".json", ".scss" ],
-		alias: {
-			src: path.join(__dirname, "src" ),
-			img: path.join(__dirname, "assets", "img")
-		}
-	},
-	watch: true,
-	module: {
-		rules: [
-			{
-				test: /\.css$/,
-				loader: "css-loader"
-			},
-			{
-				test: /\.scss$/,
-				use: [
-					{ loader: "style-loader" },
-					{ loader: "css-loader" },
-					{ loader: "sass-loader" }
-				]
-			},
-			{
-				test: /\.(woff|woff2|eot|ttf|otf)$/,
-				loader: "file-loader"
-			},
-			{
-				test: /\.(png|jpg|gif|svg)$/,
-				options: {
-					limit: 8000
-				},
-				loader: "url-loader"
-			},
-		]
-	},
-	plugins: [
-		new HtmlWebpackPlugin( {template: "./index.html"} )
-	]
-}	
+module.exports = {
+    entry: {
+        main: path.resolve(__dirname, "src", "index.js"),
+    },
+    output: {
+        filename: "[name].js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    resolve: {
+        extensions: [".js", ".json", ".scss"],
+        alias: {
+            src: path.join(__dirname, "src"),
+            img: path.join(__dirname, "assets", "img")
+        }
+    },
+    watch: true,
+    module: {
+        rules: [{
+                test: /\.css$/,
+                loader: "css-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    { loader: "style-loader" },
+                    { loader: "css-loader" },
+                    { loader: "sass-loader" }
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                loader: "file-loader"
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                options: {
+                    limit: 8000
+                },
+                loader: "url-loader"
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ template: "./index.html" })
+    ]
+}
