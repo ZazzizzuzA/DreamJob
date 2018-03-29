604648f9cb8e9d763b00875657ab8b663ddca64d
Mykhailo Pylypenko <musicvyp@gmail.com>
2018-03-29 23:12:54 +0300

    Wrong path to main.js

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,6 @@ module.exports = {
     output: {
         filename: "[name].js",
         path: path.resolve(__dirname, "dist"),
-        /* publicPath: (__dirname, "dist/"),*/
     },
     resolve: {
         extensions: [".js", ".json", ".scss", ".css"],
